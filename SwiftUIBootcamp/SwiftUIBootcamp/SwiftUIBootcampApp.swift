a1fee324c34d8778c03fe9de133938a7ee773a12
Abdullah Ansari <abdullahansari018@gmail.com>
2025-06-05 16:04:27 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Abdullah Ansari on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
