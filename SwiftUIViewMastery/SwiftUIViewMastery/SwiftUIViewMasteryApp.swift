0e46c2cf646f03f19cb4f703ade2c57bb344391c
Abdullah Ansari <abdullahansari018@gmail.com>
2025-09-05 16:57:49 +0530

    Initial commit for SwiftUIViewMastery

---
+++ SwiftUIViewMastery/SwiftUIViewMastery/SwiftUIViewMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewMasteryApp.swift
+//  SwiftUIViewMastery
+//
+//  Created by Abdullah Ansari on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
